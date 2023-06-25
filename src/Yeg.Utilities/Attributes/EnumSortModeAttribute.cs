e98f2e4483ab62e572d713726cf93b5daa35f05a
yunusemregunduz <emre.gunduz@mikro.com.tr>
2023-06-25 14:10:18 +0300

    SortModeEnum and SortModeAttribute added.

---
+++ src/Yeg.Utilities/Attributes/EnumSortModeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yeg.Utilities.Enums;
+
+namespace Yeg.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class EnumSortModeAttribute : Attribute
+    {
+        public EnumSortModeAttribute(SortModeEnum sortMode)
+        {
+            SortMode = sortMode;
+        }
+        public SortModeEnum SortMode { get; private set; }
+    }
+}
