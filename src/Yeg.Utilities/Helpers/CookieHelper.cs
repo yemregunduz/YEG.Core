91ded5a9df95eb11c1a28f875258185cab8fde72
yunusemregunduz <emre.gunduz@mikro.com.tr>
2023-06-25 14:21:23 +0300

    Added method summaries to the CookieHelper class.

--- src/Yeg.Utilities/Helpers/CookieHelper.cs
+++ src/Yeg.Utilities/Helpers/CookieHelper.cs
@@ -5,7 +5,13 @@ namespace Yeg.Utilities.Helpers
 {
     public static class CookieHelper
     {
-
+        /// <summary>
+        /// Sets a cookie with the specified name and value to be added to the HTTP response, using the provided HttpContext.
+        /// </summary>
+        /// <param name="httpContext">The HttpContext object representing the current request and response.</param>
+        /// <param name="name">The name of the cookie.</param>
+        /// <param name="value">The value of the cookie.</param>
+        /// <param name="expireMinutes">The expiration time of the cookie in minutes. Defaults to null.</param>
         public static void SetCookie(HttpContext httpContext, string name, string value, int? expireMinutes = null)
         {
             if (httpContext == null)
@@ -22,6 +28,12 @@ namespace Yeg.Utilities.Helpers
             httpContext.Response.Cookies.Append(name, value, cookieOptions);
         }
 
+        /// <summary>
+        /// Retrieves the value of the specified cookie from the provided HttpContext.
+        /// </summary>
+        /// <param name="httpContext">The HttpContext object representing the current request and response.</param>
+        /// <param name="name">The name of the cookie.</param>
+        /// <returns>The value of the cookie as a string.</returns>
         public static string GetCookieValue(HttpContext httpContext, string name)
         {
             if (httpContext == null)
@@ -33,6 +45,11 @@ namespace Yeg.Utilities.Helpers
             return httpContext.Request.Cookies[name];
         }
 
+        /// <summary>
+        /// Deletes the specified cookie using the provided HttpContext.
+        /// </summary>
+        /// <param name="httpContext">The HttpContext object representing the current request and response.</param>
+        /// <param name="name">The name of the cookie to be deleted.</param>
         public static void DeleteCookie(HttpContext httpContext, string name)
         {
             if (httpContext == null)
