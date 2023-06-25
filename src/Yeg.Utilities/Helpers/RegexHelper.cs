273ea271fef340f5372746a987d35572a2c26c7f
yunusemregunduz <emre.gunduz@mikro.com.tr>
2023-06-25 14:12:50 +0300

    Project folder stracture changed.

--- src/Yeg.Utilities/Helpers/RegexHelper.cs
+++ src/Yeg.Utilities/Helpers/RegexHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Yeg.Utilities.RegularExpressions
+namespace Yeg.Utilities.Helpers
 {
     public static partial class RegexHelper
     {
