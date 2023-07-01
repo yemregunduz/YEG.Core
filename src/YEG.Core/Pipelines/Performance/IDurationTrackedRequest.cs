4a9ff3880393b0aaa50cbfa30ada36b5990a758c
yunusemregunduz <emre.gunduz@mikro.com.tr>
2023-07-02 02:20:47 +0300

    PerformaceBehavior added.

---
+++ src/YEG.Core/Pipelines/Performance/IDurationTrackedRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YEG.Core.Pipelines.Performance
+{
+    public interface IDurationTrackedRequest
+    {
+        public int Interval { get; }
+    }
+}
